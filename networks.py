f983a69472eaaf42effaeb24f2a227149b9692a8
Davide Lobba <davidelobba99@gmail.com>
2023-10-17 15:59:42 +0000

    Minor edit

--- networks.py
+++ networks.py
@@ -1,66 +1,25 @@
 import torch
-from torchvision import models
-from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights
 import torch.nn as nn
-
-from utils import load_config
-
-'''def get_network_by_name(network_name):
-    # Add or remove network models from the dictionary as needed
-    networks = {
-        'resnet18': models.resnet18,
-        'resnet34': models.resnet34,
-        'resnet50': models.resnet50
-    }
-    return networks.get(network_name, None)()
-
-class NetworkLoader:
-    def __init__(self, args):
-        self.args = args
-
-    def load_network(self, weights_path, device):
-        network_name = self.args.network
-
-        network = get_network_by_name(network_name)
-        if network is None:
-            raise ValueError(f"Network {network_name} not recognized.")
-
-        # If the network is ResNet-like, change the final layer. Modify this logic for other architectures.
-        if "resnet" in network_name and self.args.dataset == 'cifar10':
-            num_ftrs = network.fc.in_features
-            network.fc = nn.Linear(num_ftrs, 10)
-            network.load_state_dict(torch.load(weights_path))
-            network = network.to(device)
-            network.eval()
-
-        elif "resnet" in network_name and self.args.dataset == 'imagenet':
-            num_ftrs = network.fc.in_features
-            network.fc = nn.Linear(num_ftrs, 1000)
-            weights = ResNet18_Weights.DEFAULT if network_name == 'resnet18' else ResNet34_Weights.DEFAULT if network_name == 'resnet34' else ResNet50_Weights.DEFAULT
-            network = network(weights).to(device)
-            network.eval()
-        
-
-        return network'''
-    
-
-
-import torch
-import torch.nn as nn
-from torchvision.models import resnet18, resnet34, resnet50
+from torchvision.models import resnet18, resnet34, resnet50, ResNet18_Weights, ResNet34_Weights, ResNet50_Weights
 
 class NetworkLoader():
     def __init__(self, args):
         self.network_name = args.network
         self.dataset = args.dataset
 
-    def _initialize_network(self, pretrained=False):
-        if self.network_name == 'resnet18':
-            return resnet18(pretrained=pretrained)
+    def _initialize_network(self):
+        if self.network_name == 'resnet18' and self.dataset == 'imagenet':
+            return resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        elif self.network_name == 'resnet34' and self.dataset == 'imagenet':
+            return resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
+        elif self.network_name == 'resnet50' and self.dataset == 'imagenet':
+            return resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+        elif self.network_name == 'resnet18':
+            return resnet18(weights=None)
         elif self.network_name == 'resnet34':
-            return resnet34(pretrained=pretrained)
+            return resnet34(weights=None)
         elif self.network_name == 'resnet50':
-            return resnet50(pretrained=pretrained)
+            return resnet50(weights=None)
         else:
             raise ValueError(f"Network {self.network_name} not recognized.")
     
@@ -77,11 +36,7 @@ class NetworkLoader():
             raise ValueError(f"Dataset {self.dataset} not supported.")
 
     def load_network(self, weights_path=None, device='cpu'):
-        if self.dataset == 'imagenet':
-            network = self._initialize_network(pretrained=True)
-        else:
-            network = self._initialize_network(pretrained=False)
-            
+        network = self._initialize_network()    
         self._adjust_output_layer(network)
         
         if self.dataset == 'cifar10' and weights_path:
